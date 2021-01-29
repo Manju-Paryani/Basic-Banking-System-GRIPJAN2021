3401eeb78fdcaace1cfe2dfa9cd7637728a795c3
Manju-Paryani <66443581+Manju-Paryani@users.noreply.github.com>
2021-01-29 19:32:18 +0530

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -1,7 +1,11 @@
 
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "bank");
+// Development Connections
+//$conn = mysqli_connect("localhost", "root", "", "bank");
+
+//Remote Database connection
+$conn = mysqli_connect("remotemysql.com", "XOBff6jh1u", "zLwpPRb8wj", "XOBff6jh1u");
 
 if (!$conn) {
   die("Could not connect to the database due to the following error --> " . mysqli_connect_error());
